c52c2869946b00965ef745e443de293acccb8520
valentino-dev <angelo.valentino.brade@gmail.com>
2024-03-03 21:50:03 +0100

    getestdata

---
+++ bash/genTestData.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+nvcc src/simulation.cu -o "bin/simulation.out"
+a=0.5
+m0=0.5
+lambda=0.0
+mu=2.0
+runs=1
+n=10
+SII=5
+RS=50
+TpB=512
+MCS=136
+MCI=500
+xjpT=2
+f=0.0
+nvprof bin/simulation.out "data/data_fig5_check.csv" "${runs}" "${SII}" "${RS}" "${xjpT}" "${n}" "${TpB}" "${MCS}" "${MCI}" "${a}" "${m0}" "${lambda}" "${mu}" "${f}"
