4847e63aea6162db857e1f12e61c6bdc2041c52b
valentino-dev <angelo.valentino.brade@gmail.com>
2024-03-19 22:32:19 +0100

    correlation error

--- bash/genTestData.sh
+++ bash/genTestData.sh
@@ -4,15 +4,15 @@
 nvcc src/simulation.cu -o "bin/simulation.out"
 runs=1
 SII=1
-RS=1000
+RS=100
 xjpT=2
 n=10
-TpB=512
+TpB=32
 MCS=1
-MCI=10000
+MCI=100
 a=0.5
 m0=0.5
 lambda=0.0
 mu=2.0
 f=0.0
-nvprof bin/simulation.out "data/data_fig5_10k_RS_1k.csv" "${runs}" "${SII}" "${RS}" "${xjpT}" "${n}" "${TpB}" "${MCS}" "${MCI}" "${a}" "${m0}" "${lambda}" "${mu}" "${f}"
+nvprof bin/simulation.out "data/data_fig5_100k_RS_100_N_64.csv" "${runs}" "${SII}" "${RS}" "${xjpT}" "${n}" "${TpB}" "${MCS}" "${MCI}" "${a}" "${m0}" "${lambda}" "${mu}" "${f}"
---
+++ src/processing/processing.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import dask.dataframe as dd
+import scipy.odr
+from olib import *
+
+plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
+
+#path = "data/data_fig5_1k_RS_100.csv"
+path = "data/data_fig5_10k_RS_1k.csv"
+
+def autoCorrelationErrorPerBinStep():
+    data = dd.read_csv(path, sep=";", header=None).to_dask_array(lengths=True)
+    correlation = data * np.roll(data, 1, 1)
+
+    std_of_bins = np.zeros(int(np.log10(correlation.shape[0])))
+    correlation_bined_mean = correlation
+    for i in range(std_of_bins.shape[0]):
+        print("i:", i)
+        print(std_of_bins.shape[0]-i)
+        correlation_bined = correlation_bined_mean.reshape(10**(std_of_bins.shape[0]-i-1), 10, correlation_bined_mean.shape[1])
+        print(correlation_bined.shape)
+        correlation_bined_mean = correlation_bined.mean(1)
+        print(correlation_bined_mean.shape)
+        std = np.std(correlation_bined_mean, 0)
+        std_of_bins[i] = std.mean()
+
+    X = np.arange(0, std_of_bins.shape[0])
+    Xerr = np.zeros_like(X)
+    Y = std_of_bins
+    Yerr = np.zeros_like(Y)
+    table = Table(X, Xerr, Y, Yerr, "Correlation Error", "bin step", r"$\sigma_x$")
+
+    fig, ax = plt.subplots()
+    ax = plotData(ax, table, polyfit=0, fmt="x")
+    ax = setSpace(ax, table)
+    plt.savefig("plots/CorrelationError.pdf", dpi=500)
+
+def autoCorrelationErrorPerBinSize():
+    data = dd.read_csv(path, sep=";", header=None).to_dask_array(lengths=True)
+    correlation = data * np.roll(data, 1, 1)
+
+    std_of_bins = np.zeros(int(np.log10(correlation.shape[0]))+1)
+    for i in range(std_of_bins.shape[0]):
+        print("i:", i)
+        print(std_of_bins.shape[0]-i)
+        correlation_bined = correlation.reshape(10**(std_of_bins.shape[0]-i-1), 10**i, correlation.shape[1])
+        print(correlation_bined.shape)
+        correlation_bined_mean = correlation_bined.mean(1)
+        print(correlation_bined_mean.shape)
+        std = np.std(correlation_bined_mean, 0)
+        std_of_bins[i] = std.mean()
+
+    X = np.arange(0, std_of_bins.shape[0])
+    Xerr = np.zeros_like(X)
+    Y = std_of_bins
+    Yerr = np.zeros_like(Y)
+    table = Table(X, Xerr, Y, Yerr, "Correlation Error per bin size", "bin size", r"$\sigma_x$")
+
+    fig, ax = plt.subplots()
+    ax = plotData(ax, table, polyfit=0, fmt="x")
+    ax = setSpace(ax, table)
+    plt.savefig("plots/CorrelationErrorPerBinSize.pdf", dpi=500)
+
+
+autoCorrelationErrorPerBinSize()
